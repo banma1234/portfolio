0e99e6b285de79047e45088bff2d8bb0ed7f33e2
chocoham <banma1234@gmail.com>
2024-12-30 02:24:10 +0900

    fix: 여백 제거 및 일부 버그 수정

--- src/script/global.js
+++ src/script/global.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   let currentScroll = 0; // 현재 스크롤 위치
   let targetScroll = 0; // 목표 스크롤 위치
-  const scrollSpeed = 0.1; // 부드럽게 스크롤하는 속도
+  const scrollSpeed = 0.05; // 부드럽게 스크롤하는 속도
   let debounceTimeout;
 
   // 스크롤 이벤트로 목표 위치 업데이트
   window.addEventListener(
     "wheel",
-    event => {
+    (event) => {
       event.preventDefault();
       clearTimeout(debounceTimeout);
       debounceTimeout = setTimeout(() => {
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
           0,
           Math.min(
             targetScroll,
-            document.body.scrollHeight - window.innerHeight,
-          ),
+            document.body.scrollHeight - window.innerHeight
+          )
         );
       }, 10);
     },
-    { passive: false },
+    { passive: false }
   );
 
   // 스크롤 애니메이션
